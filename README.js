7928824eb0423ddd4cf43b74b20e92be46d26c8e
Thiago Skibinski <thiagoskibinski@gmail.com>
2020-05-22 09:32:32 -0300

    Rename README.js to README.md

--- README.js
+++
@@ -1,5 +0,0 @@
-### SNAKE GAME JS
-
-this is a study case to develop games only with JS
-
-- To execute this project, just download the repository and open index.html in your browser
---
+++ README.md
@@ -0,0 +1,5 @@
+### SNAKE GAME JS
+
+this is a study case to develop games only with JS
+
+- To execute this project, just download the repository and open index.html in your browser
